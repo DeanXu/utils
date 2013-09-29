195b5ac221c7abe285b2f50e561abccc4e65e219
DeanXu <xdx158@gmail.com>
2013-09-29 16:30:38 +0800

    update

--- install.sh
+++ install.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+apt-get update
+apt-get install -y vim
+apt-get install -y zsh
+wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
+
+
 cp vim/vimrc ~/.vimrc
 cp vim/dot_vim ~/.vim
+cp vim/distinguished.vim /usr/share/vim/vim??/colors/distinguished.vim
 
-apt-get update
-apt-get install -y zsh
-apt-get install -y vim
-wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
 
 cp zsh/zshrc ~/.zshrc
 cp zsh/bash_aliases  ~/.bash_aliases 
